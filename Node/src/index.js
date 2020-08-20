4e940d17ce71dd4acfe57067296e398b0517c7bd
mwlk <mirkoivowlk@gmail.com>
2020-08-20 00:26:46 -0300

    agregando proyecto nodejs

---
+++ Node/src/index.js
@@ -0,0 +1,23 @@
+import { createServer } from 'http';
+
+var server = createServer(function(request, response){
+
+    if(request.url==='/'){
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write('<html><body><p>Home</p></body></html>');
+        response.end();
+    }else if (request.url === '/exit'){
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write('<html><body><p>Exit</p></body></html>');
+        response.end();
+    }else {
+        response.writeHead(404,{'Content-Type':'text/html'});
+        response.write('<html><body><p>not found</p></body></html>');
+        response.end();
+    }
+    
+});
+
+server.listen(4000);
+
+console.log("running")
