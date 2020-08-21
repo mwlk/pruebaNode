94042730f3711a87870dc92f26409786b1b1bd87
mwlk <mirkoivowlk@gmail.com>
2020-08-21 01:00:45 -0300

    format

--- Node/src/index.js
+++ Node/src/index.js
@@ -1,23 +1,21 @@
 var http = require("http");
 
-var server = http.createServer(function(request, response) {
-
-    if(request.url==='/'){
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write('<html><body><p>Home</p></body></html>');
-        response.end();
-    }else if (request.url === '/exit'){
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write('<html><body><p>Exit</p></body></html>');
-        response.end();
-    }else {
-        response.writeHead(404,{'Content-Type':'text/html'});
-        response.write('<html><body><p>not found</p></body></html>');
-        response.end();
-    }
-    
+var server = http.createServer(function (request, response) {
+  if (request.url === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<html><body><p>Home</p></body></html>");
+    response.end();
+  } else if (request.url === "/exit") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<html><body><p>Exit</p></body></html>");
+    response.end();
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.write("<html><body><p>not found</p></body></html>");
+    response.end();
+  }
 });
 
 server.listen(4000);
 
-console.log("Corriendo proyecto")
+console.log("Corriendo proyecto");
